37ae9b9ac2f99270846fc3958bb19a7ca8ceaf1d
Muhammad Al Shurbaji <114419191+mshurbji@users.noreply.github.com>
2026-04-01 01:13:07 +0200

    Initialize main.py to run knowledge base and chatbot

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.build_knowledge_base import build_knowledge_base
+from src.semantic_search import run_semantic_search
+from src.chatbot import run_chatbot
+
+if __name__ == "__main__":
+    build_knowledge_base()
+    run_semantic_search()
+    run_chatbot()
